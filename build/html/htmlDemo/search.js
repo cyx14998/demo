1c62bfb86a1116cff65a6ed27bf8ea26da48ee94
cyx008 <915976779@qq.com>
2017-09-14 11:28:27 +0800

    update crlf

--- build/html/htmlDemo/search.js
+++ build/html/htmlDemo/search.js
@@ -4,54 +4,54 @@ const fs = require("fs");
 const [$ = process.exit()] = [process.argv[2]];
 const reg =
     $.startsWith("I")
-    ? RegExp($.slice(1), "u")
-    : RegExp($, "iu");
+        ? RegExp($.slice(1), "u")
+        : RegExp($, "iu");
 
 const visit = path => {
     const list = [];
-    for(let name of fs.readdirSync(path)){
-        try{
+    for (let name of fs.readdirSync(path)) {
+        try {
             const pathname = path + name;
-            if(fs.statSync(pathname).isDirectory()){
-                if(reg.test(name)){
+            if (fs.statSync(pathname).isDirectory()) {
+                if (reg.test(name)) {
                     log(pathname + "/");
                     list.push(...visit(pathname + "/"));
-                }else{
+                } else {
                     list.push(pathname + "/");
                 }
-            }else if(reg.test(name)) log(pathname);
-        }catch(error){}
+            } else if (reg.test(name)) log(pathname);
+        } catch (error) { }
     }
     return list.length === 1 ? visit(list[0]) : list;
 };
 
-const search = function*(path){
-    try{
+const search = function* (path) {
+    try {
         const head = {};
         let p = head;
-        for(let i of visit(path)){
-            p = p.next = {value: search(i)};
+        for (let i of visit(path)) {
+            p = p.next = { value: search(i) };
         };
-        while(head.next){
+        while (head.next) {
             p = head;
-            while(p.next){
+            while (p.next) {
                 yield;
-                if(p.next.value.next().done){
+                if (p.next.value.next().done) {
                     p.next = p.next.next;
-                }else{
+                } else {
                     p = p.next;
                 }
             }
         }
         yield;
-    }catch(error){}
+    } catch (error) { }
 };
 
 let t2;
 const log = msg => {
-    if(t2){
+    if (t2) {
         t2 = Date.now();
-    }else{
+    } else {
         t2 = Date.now();
         console.log("找出第一条结果的用时：" + (t2 - t1) + "ms");
     }
@@ -59,6 +59,6 @@ const log = msg => {
 };
 
 const t1 = Date.now();
-for(let i of search("./"));
+for (let i of search("./"));
 
-if(t2) console.log("从开始搜索到找出最后一条结果的用时：" + (t2 - t1) + "ms");
+if (t2) console.log("从开始搜索到找出最后一条结果的用时：" + (t2 - t1) + "ms");
